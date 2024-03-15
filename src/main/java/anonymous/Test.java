62e41e1e9447619d93c72fa90e5d9b1839165286
Shahadoth-ali <md.sahadat.live@gmail.com>
2024-03-15 23:08:57 +0600

    final1 Commit

--- src/main/java/anonymous/Test.java
+++ src/main/java/anonymous/Test.java
@@ -3,7 +3,9 @@ package anonymous;
 
 public class Test {
     public static void main(String[] args) {
+        //ekhane object er sathei class create korchi
         Person p=new Person(){
+            
             @Override
             void display(){
                 System.out.println("test class");
